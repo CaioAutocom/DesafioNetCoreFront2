e5c89ed7cefa40af88a31d18109ab8165174cd48
Caio Mendes <caio.mendes@autocom-mg.com.br>
2024-03-06 17:58:17 -0300

    Configurado default options de pipe para localidade pt-br e exibição de data.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,24 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { LoginComponent } from './components/layout/login/login.component';
 import { ComponentsModule } from './components/components.module';
 import { ResolveIconAlignmentDirective } from './directives/resolve-icon-alignment.directive';
+import { DATE_PIPE_DEFAULT_OPTIONS, registerLocaleData } from '@angular/common';
+import { LOCALE_ID } from '@angular/core';
 
+import localePt from '@angular/common/locales/pt';
+
+const datePipeConfig =  {
+  provide: DATE_PIPE_DEFAULT_OPTIONS,
+  useValue: {
+    dateformat: 'dd/MM/yyyy',
+    timezone: '+0000',
+  }
+};
+
+const localePipe = {
+  provide: LOCALE_ID,
+  useValue: 'pt-BR'
+}
+registerLocaleData(localePt, 'pt-BR');
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,7 +38,10 @@ import { ResolveIconAlignmentDirective } from './directives/resolve-icon-alignme
     ComponentsModule
   ],
   providers: [
-    provideAnimationsAsync()
+    localePipe,
+    datePipeConfig,
+    provideAnimationsAsync(),
+    
   ],
   bootstrap: [AppComponent]
 })
